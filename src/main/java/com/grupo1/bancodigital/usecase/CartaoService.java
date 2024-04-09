436b734e01c2173a4552a75f8c3475d5061c0a6d
Patrick Eispu Simões <patrickeispu.arc@gmail.com>
2024-04-09 17:06:39 -0300

    Criacao de endpoint para criacao de cartao de debito e credito(nao esta funcional)

--- src/main/java/com/grupo1/bancodigital/usecase/CartaoService.java
+++ src/main/java/com/grupo1/bancodigital/usecase/CartaoService.java
@@ -1,5 +1,7 @@
 package com.grupo1.bancodigital.usecase;
 
+import com.grupo1.bancodigital.dto.CartaoRequest;
+import com.grupo1.bancodigital.dto.CartaoResponse;
 import com.grupo1.bancodigital.model.cartao.CartaoEntity;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,11 @@ import org.springframework.stereotype.Service;
 public class CartaoService {
 
 
+    public static CartaoResponse cadastrarCartao(CartaoRequest cartao) {
+
+
+    return null;
+    }
 
 
 
